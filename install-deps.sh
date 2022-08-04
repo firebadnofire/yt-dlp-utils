2d5146478e91382b8b5c42acdb10efa3cd12082c
firebadnofire <firebadnofire@icloud.com>
2022-08-03 22:42:16 -0400

    added install-deps and created checks for dependancies

---
+++ install-deps.sh
@@ -0,0 +1,20 @@
+while true
+do
+        read -r -p "Do you want to install ffmpeg and yt-dlp? (debian & ubuntu) y/n " input
+
+        case $input in
+            [yY][eE][sS]|[yY])
+                        sudo apt install ffmpeg yt-dlp
+                        break
+                        ;;
+            [nN][oO]|[nN])
+                        sleep 1s
+                        break
+                        ;;
+            *)
+                echo "Please answer yes or no"
+                ;;
+        esac
+done
+    exit
+fi
