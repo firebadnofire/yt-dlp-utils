3c555dfed4e19d99425c49c83fe9a020a69b485d
firebadnofire <firebadnofire@icloud.com>
2022-08-03 23:07:13 -0400

    Improved install-deps.sh to include most OSes

--- install-deps.sh
+++ install-deps.sh
@@ -1,21 +1,35 @@
+#!/usr/bin/env bash
 #installs the dependancies needed
-while true
-do
-        read -r -p "Do you want to install ffmpeg and yt-dlp? (debian & ubuntu) y/n " input
+echo 'Choose your option below, to continue:
+1 Debian/Ubuntu
+2 Arch Linux
+3 Fedora
+4 install with pip (requires ~/.local/bin to be in the path variable and pip)
+5 Exit Installation'
+# Function for $errors:
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+read number && echo "selected"
+case $number in
 
-        case $input in
-            [yY][eE][sS]|[yY])
-                        sudo apt install ffmpeg yt-dlp
-                        break
-                        ;;
-            [nN][oO]|[nN])
-                        sleep 1s
-                        break
-                        ;;
-            *)
-                echo "Please answer yes or no"
-                ;;
-        esac
-done
-    exit
-fi
+    1)
+ 	sudo apt-get install yt-dlp ffmpeg 
+	;;
+    2)
+	sudo pacman -S --needed yt-dlp ffmpeg
+	;;
+    3)
+	sudo dnf install yt-dlp ffmpeg
+	;;
+    4)
+	pip install yt-dlp ffmpeg
+	;;
+    5)
+        echo "Installation Cancelled"
+        exit
+        ;;
+
+    *) echo "invalid option '$number'";;
+esac
