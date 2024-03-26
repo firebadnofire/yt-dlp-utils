e43b0de54c9384b1d1b6375659d49d88f2a4864c
firebadnofire <william@archuser.org>
2024-03-26 08:44:40 -0400

    commit

--- windows/yt-music.cmd
+++ windows/yt-music.cmd
@@ -3,7 +3,8 @@ REM Define colors
 set "RED=\033[0;31m"
 set "NC=\033[0m"
 set "GREEN=\033[0;32m"
-
+set current_dir=%CD%
+echo Current directory: %current_dir%
 
 echo Checking if yt-dlp.exe and ffmpeg.exe exist...
 
@@ -35,3 +36,54 @@ if "%latest%"=="%current%" (
     exit /b 1
 )
 
+set "PFILE=%APPDATA%\ytdlputils"
+set "FILE=%APPDATA%\ytdlputils\ytdlpmusic.conf"
+
+echo Checking for default save path...
+timeout /t 1 >nul
+
+if exist "%FILE%" (
+    for /f "usebackq delims=" %%I in ("%FILE%") do set "change=%%I"
+) else (
+    echo %FILE% not found
+    timeout /t 1 >nul
+    mkdir "%PFILE%"
+    set /p "rootdir=Input directory you want to save music files to by default (Use full path name ex: C:\Users\YOURUSERNAME\Downloads): "
+    mkdir "%rootdir%"
+    (
+        echo. REM this file was made to ensure that yt-dlp knows where to save your music
+        echo. REM edit this directory if you wish to change it
+        echo %rootdir%
+    ) >"%FILE%"
+    for /f "usebackq delims=" %%I in ("%FILE%") do set "change=%%I"
+)
+
+echo OK
+cd /d "%change%"
+echo You will download to:
+cd
+:loop
+set /p "input=Do you want to add a directory to put your song(s) in? [y/n] "
+if /i "%input%"=="y" (
+    set /p "dir=Input directory name: "
+    mkdir "%dir%" >nul 2>nul
+    if not exist "%dir%" (
+        echo Failed to create directory.
+    ) else (
+        cd /d "%dir%"
+        echo You will now download to:
+        cd
+        goto :endloop
+    )
+) else if /i "%input%"=="n" (
+    goto :endloop
+) else (
+    echo Please answer yes or no.
+)
+goto :loop
+
+:endloop
+echo Input YouTube URL.
+set /p "url="
+%current_dir%\yt-dlp.exe -4 -x --audio-format mp3 --embed-thumbnail --add-metadata %url%
+cd %current_dir%
